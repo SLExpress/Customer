b571ca3cf671606255ce08cbe179d6c171d6ce6f
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-06-30 18:29:22 +0530

    thirtyth commit

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -33,7 +33,7 @@ axios.interceptors.response.use(null, (error) => {
       icon: "error",
       title: "404 Not Found",
     });
-  } else if (error.response.status === 500) {
+  } else if (error.response.status === 501) {
     auth.logout();
     window.location = "/expired";
   } else return Promise.reject(error);
